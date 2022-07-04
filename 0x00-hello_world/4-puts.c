cf099888957b3df402d4728dcc6d349baff87129
YvonneKoniry <yvonnekoniry@gmail.com>
2022-07-04 07:16:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
-int main(void){puts ("/"Programming is like building a multilingual puzzle");
-return (0);
+/**
+ * main-entry point
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+return(0);
 }
