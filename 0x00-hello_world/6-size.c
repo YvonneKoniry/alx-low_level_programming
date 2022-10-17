04d891f8b896cf7f61a7b2905d96a2fd4c8300f2
YvonneKoniry <yvonnekoniry@gmail.com>
2022-10-17 13:53:45 +0000

    declaring size of different data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,14 @@
 */
 int main(void)
 {
-	printf("size of char: %lu bytes\n", [unsigned long]size of (d)];
-	printf("size of int: %lu bytes\n", [unsigned long]size of (d)];
-	printf("size of long int: %lu bytes\n", [unsigned long]size of (d)];
-	printf("size of long long: %lu bytes\n", [unsigned long]size of (d)];
-	printf("size of float: %lu bytes\n", [unsigned long]size of (d)];
+	int d;
+
+	printf("size of char: %lu bytes\n", (unsigned long)sizeof (d));
+	printf("size of int: %lu bytes\n", (unsigned long)sizeof (d));
+	printf("size of long int: %lu bytes\n", (unsigned long)sizeof (d));
+	printf("size of long long: %lu bytes\n", (unsigned long)sizeof (d));
+	printf("size of float: %lu bytes\n", (unsigned long)sizeof (d));
 
 	return (0);
+}
 
