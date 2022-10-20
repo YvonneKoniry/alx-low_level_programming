c0a784ea7f7334945e8b67f893a25bd792ff2319
YvonneKoniry <yvonnekoniry@gmail.com>
2022-10-20 21:20:40 +0000

    print alphabets times 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints 10times the alphabet in lower case
+*Return:0 -success
+*/
+
+void print_alphabet_x10(void)
+{
+	int ten;
+	char la;
+
+	for (ten = 0; ten <= 10; ten++)
+
+	{
+		for (la = 'a'; la <= 'z'; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
+
