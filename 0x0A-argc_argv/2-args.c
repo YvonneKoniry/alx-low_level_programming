45ad4a7e03ca402c585418dcfddb03db1a5866b6
YvonneKoniry <yvonnekoniry@gmail.com>
2022-11-07 21:17:47 +0000

    program that prints all arguments it receives

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+*main - program that prints all arguments it receives
+*@argv: arguments passed
+*@argc:argument count
+*/
+int main(int argc, char *argv[])
+{
+
+	(void)argv;
+	printf("%d\n", (argc - 1));
+	return (0);
+}
