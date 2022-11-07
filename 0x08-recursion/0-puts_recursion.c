0be2c5a65b32771c7a236ec41e6d09ede22c1420
YvonneKoniry <yvonnekoniry@gmail.com>
2022-11-07 15:44:05 +0000

    string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,8 +2,8 @@
 
 /**
 *_puts_recursion - prints a string followed by a newline
-*@s: pointer to string
-*Return:0
+*@s: pointer to string to be printed
+*Return:void
 */
 
 void _puts_recursion(char *s)
@@ -11,7 +11,8 @@ void _puts_recursion(char *s)
 		if (*s)
 
 		{
-			_putchar(*s + 1);
+			_putchar(*s);
+			_puts_recursion(s + 1);
 		}
 		else
 			_putchar('\n');
