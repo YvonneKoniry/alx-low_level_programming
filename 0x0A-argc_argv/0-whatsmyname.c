5bc4ab0db0bbc960335ad33ca2ad48db9f886f17
YvonneKoniry <yvonnekoniry@gmail.com>
2022-11-07 19:07:41 +0000

    using argc and argv to print name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - program that prints its name, followed by a new line
+*@argc:counter for commad line arguments passed by user
+*@argv: array of character pointers listing the arguements
+*Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[1]);
+	return(0);
+}
+	
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+learning about arguements to main
