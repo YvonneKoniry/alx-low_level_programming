c61673daef13b2f053f2761b0dadb782dfef5c38
YvonneKoniry <yvonnekoniry@gmail.com>
2022-11-07 21:22:46 +0000

    ./args You can do anything, but not everything.

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,11 +4,16 @@
 *main - program that prints all arguments it receives
 *@argv: arguments passed
 *@argc:argument count
+*Return:0
 */
 int main(int argc, char *argv[])
 {
 
-	(void)argv;
-	printf("%d\n", (argc - 1));
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
