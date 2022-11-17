21454efaaeaef739bdc41e36e78a6ebb51b7d3f9
YvonneKoniry <yvonnekoniry@gmail.com>
2022-11-17 09:54:53 +0000

    dynamically allocating memory to string created

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*create array - te create an array of chars
+*create_array - to create an array of chars
 *@size: size of unsigned int
 *@c: string of characters to be inputted
 *Return:Null if size = 0/ fail,else pointer to array
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 
 /*checking if size is zero*/
-	if(size == 0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
@@ -24,17 +24,14 @@ char *create_array(unsigned int size, char c)
 	ptr = (char *)malloc(size * sizeof(char));
 
 /*return null if space to be allocated wasn't created*/
-	if(ptr == NULL)
+	if (ptr == NULL)
 	{
 		return (NULL);
 	}
 /*replacing all empty spaces with c*/
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		*(ptr + i) = c;
 	}
 	return (ptr);
 }
-	
-
-
